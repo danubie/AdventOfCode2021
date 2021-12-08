3794674ddbff0fe9a1c06f947de46f560e8bc8be
danubie <w_wagner62@hotmail.com>
2021-12-08 20:20:56 +0100

    add param to serve Part2 as well

--- 07_TheTreasury.ps1
+++ 07_TheTreasury.ps1
@@ -1,18 +1,27 @@
 function ReadInputData ($fileName, $inputData) {
     (Get-Content $filename) -Split ',' | ForEach-Object { [void] $inputData.Add([int]$_) }
 }
-function CalcCostForPosition ($TargetPosition, $inputData) {
+function CalcCostForPosition ($TargetPosition, $inputData, $Part) {
     $cost = 0
     $inputData.ForEach({
         $currPosition = $_
-        $currCost = [math]::Abs( $currPosition - $TargetPosition )
-        $cost += $currCost
+        if ($currPosition -ne $TargetPosition) {
+            if ($Part -eq 1) {
+                $cost += [math]::Abs($currPosition - $TargetPosition)
+            } else {
+                $sum = 0
+                for ($move = 0; $move -le [math]::Abs($currPosition - $TargetPosition); $move++) {
+                    $sum += $move
+                }
+                # Write-Warning "currPosition: $currPosition; TargetPosition: $TargetPosition; sum: $sum"
+                $cost += $sum
+            }
+        }
     })
     return $cost
 }
 
-function GetLeastCostPosition ($inputData) {
-    $filename = "$PSScriptRoot\07_TheTreasury-Sample.txt"
+function GetLeastCostPosition ($filename, $Part) {
     [System.Collections.ArrayList]$InputData = @()
     ReadInputData $filename $InputData
 
@@ -20,14 +29,15 @@ function GetLeastCostPosition ($inputData) {
     $minCost = [int32]::MaxValue
     $minCostPosition = -1
     for ($i = 0; $i -lt $InputData.Count; $i++) {
-        $ret = CalcCostForPosition $i $InputData
+        $ret = CalcCostForPosition $i $InputData $Part
         [void] $listCosts.Add($ret)
-        Write-Warning "Cost for position $i is $ret"
+        # Write-Warning "Cost for position $i is $ret"
         if ($ret -lt $minCost) {
             $minCost = $ret
             $minCostPosition = $i
         }
     }
     Write-Warning "Least cost position is $minCostPosition with cost $minCost"
+    return $minCost
 }
 
